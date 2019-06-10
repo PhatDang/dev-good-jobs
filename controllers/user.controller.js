4cbc80dcb740fe5de96f01ec7186301a6c012fb1
phat.dm <phat.dm@komit-consulting.com>
2019-06-10 21:47:14 +0700

    [REF] Refactor code project and modules

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,15 +1,15 @@
 //
 //
-//
-var users =  [
+//================================
+var users = [
     {
-        id:1,
+        id: 1,
         username: "0707144248",
         password: "a",
         category: 1, //This man wanna find a job !!
     },
     {
-        id:2,
+        id: 2,
         username: "0979679671",
         password: "a",
         category: 2, //This man wanna create jobs !!
@@ -22,13 +22,13 @@ module.exports.signIn = (req, res) => {
 module.exports.signUp = (req, res) => {
     res.render('users/signup');
 }
-module.exports.signInValid = (req,res) => {
+module.exports.signInValid = (req, res) => {
     var user = req.body.phone;
     var pass = req.body.password;
-    for (let i = 0; i<users.length; i++) {
-        if (user === users[i].username && pass ===users[i].password) {
-            if(users[i].category === 1) {
-                res.render('category/findjob',users[i]);
+    for (let i = 0; i < users.length; i++) {
+        if (user === users[i].username && pass === users[i].password) {
+            if (users[i].category === 1) {
+                res.render('category/findjob', users[i]);
             } else {
                 res.render('category/createjob', users[i])
             }
--- routes/category.route.js
+++ routes/category.route.js
@@ -1,11 +1,13 @@
 //
 //
-//
+//================================
 const express = require('express');
 const router = express.Router();
 const ctrlCategory = require('../controllers/category.controller');
 const ctrlHome = require('../controllers/user.controller');
+
 router.get('/nguoi-tim-viec', ctrlCategory.findjob);
+
 router.get('/viec-tim-nguoi', ctrlCategory.createjob);
 
 module.exports = router;
