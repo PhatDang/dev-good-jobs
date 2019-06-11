3c0504c362070e15a25e7b3923472ab34b5245e8
duclux <duc.lux47@gmail.com>
2019-06-11 23:31:28 +0700

    [UPDATE] Create dir for js ux/ui

---
+++ static/js/validation.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var userName = document.getElementById('phone-input-log');
+    var password = document.getElementById('pass-input.log');
+})
