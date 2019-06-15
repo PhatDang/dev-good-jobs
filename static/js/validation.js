7050e85b05899d88e0c295d1a0df73ca0984db5a
nguyenduc308 <duc.lux47@gmail.com>
2019-06-15 12:37:15 +0700

    [update] UX/UI Validation

--- static/js/validation.js
+++ static/js/validation.js
@@ -1,4 +1,20 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var userName = document.getElementById('phone-input-log');
-    var password = document.getElementById('pass-input.log');
-})
+let errorList = ["Bạn phải nhập số điện thoại / mật khẩu","Số điện thoại không hợp lệ","Mật khẩu không hợp lệ"]
+let userNameElement = document.querySelector('#phone-log');
+let passWordElement = document.querySelector('#pass-log');
+let messengerElement = document.querySelector('.messenger');
+function clickSubmit() {
+    let userName = userNameElement.value;
+    let passWord = passWordElement.value;
+    if (!userName ||!passWord) {
+        messengerElement.innerHTML = errorList[0] ;
+        return false;
+    }else if (userName.length !== 10) {
+        messengerElement.innerHTML = errorList[1];
+        return false;
+    }else if (passWord.length < 6) {
+        messengerElement.innerHTML = errorList[2] ;
+        return false;
+    }else {
+        return true;
+    }
+}
