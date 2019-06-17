6fbd8e2718a0b8210b56b68f32defe7d4eb11ce6
phat.dm <phat.dm@komit-consulting.com>
2019-06-17 13:26:54 +0700

    [REF] Optimized code

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,40 +1,35 @@
 //
 //
 //================================
-var users = [
-    {
-        id: 1,
-        userName: "0707144248",
-        firstName: "Phat",
-        lastName: "Dang Minh",
-        passWord: "123456",
-        category: 1, //This man wanna find a job !!
-    },
-    {
-        id: 2,
-        userName: "0979679671",
-        firstName: "Nguyen Huu",
-        lastName: "Duc",
-        passWord: "123456",
-        category: 2, //This man wanna create jobs !!
-    },
-    {
-        id: 3,
-        userName: "0937475448",
-        firstName: "Le Minh",
-        lastName: "Trung",
-        passWord: "123456",
-        category: 2, //This man wanna create jobs !!
-    },
-    {
-        id: 4,
-        userName: "0926609866",
-        firstName: "Nguyen",
-        lastName: "Hung",
-        passWord: "123456",
-        category: 1, //This man wanna find a job !!
-    },
-]
+var users = [{
+    id: 1,
+    userName: "0707144248",
+    firstName: "Phat",
+    lastName: "Dang Minh",
+    passWord: "123456",
+    category: 1, //This man wanna find a job !!
+}, {
+    id: 2,
+    userName: "0979679671",
+    firstName: "Nguyen Huu",
+    lastName: "Duc",
+    passWord: "123456",
+    category: 2, //This man wanna create jobs !!
+}, {
+    id: 3,
+    userName: "0937475448",
+    firstName: "Le Minh",
+    lastName: "Trung",
+    passWord: "123456",
+    category: 2, //This man wanna create jobs !!
+}, {
+    id: 4,
+    userName: "0926609866",
+    firstName: "Nguyen",
+    lastName: "Hung",
+    passWord: "123456",
+    category: 1, //This man wanna find a job !!
+}, ]
 var s = {};
 module.exports.signIn = (req, res) => {
     res.render('users/signin');
