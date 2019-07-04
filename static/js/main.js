704fec9d9e1ef0207679f5d2b41cfa0356dbd4aa
duc <duc.lux45@gmail.com>
2019-07-05 00:59:27 +0700

    login-front-end valid

--- static/js/main.js
+++ static/js/main.js
@@ -1,34 +1,3 @@
-// //Switch Tab
-// var tabs = document.getElementsByClassName('tab');
-// function tabSwitch(element) {
-//     for (var i=0; i<tabs.length;i++) {
-//         tabs[i].setAttribute('class', 'tab');
-//     }
-//     element.setAttribute('class','tab active');
-// }
-// // UX/UI Validation
-// let errorList = ["Bạn phải nhập số điện thoại / mật khẩu","Số điện thoại không hợp lệ","Mật khẩu không hợp lệ"]
-// let userNameElement = document.querySelector('#phone-log');
-// let passWordElement = document.querySelector('#pass-log');
-// let messengerElement = document.querySelector('.messenger');
-// function clickSubmit() {
-//     let userName = userNameElement.value;
-//     let passWord = passWordElement.value;
-//     if (!userName ||!passWord) {
-//         messengerElement.innerHTML = errorList[0] ;
-//         return false;
-//     }else if (userName.length !== 10) {
-//         messengerElement.innerHTML = errorList[1];
-//         return false;
-//     }else if (passWord.length < 6) {
-//         messengerElement.innerHTML = errorList[2] ;
-//         return false;
-//     }else {
-//         return true;
-//     }
-// }
-
-
 //Switch Box login and Reg modal
 const regElem = document.getElementById('reg-modal');
 const logInElem = document.getElementById('login-modal');
@@ -40,3 +9,48 @@ function loginModal() {
     logInElem.style.display='block';
     regElem .style.display='none';
 }
+function outModal() {
+    logInElem.style.display='none';
+    regElem .style.display='none';
+}
+//Validation
+const getInputPass = document.querySelector("#reg_form #password");
+const getInputPass2 = document.querySelector("[name='password2']")
+const tooltip = document.querySelector(".tooltips");
+const err1 = document.querySelector("[err='1']");
+const err2 = document.querySelector("[err='2']");
+const err3 = document.querySelector("[err='3']");
+const err4 = document.querySelector("[err='4']");
+const err5 = document.querySelector("[err='5']");
+const err7 = document.querySelector("[err='7']");
+getInputPass.addEventListener('focus',() => {
+    tooltip.setAttribute('class', 'tooltips show');
+})
+getInputPass.addEventListener('blur', () => {
+    tooltip.setAttribute('class', 'tooltips');
+})
+getInputPass.addEventListener('change', () => {
+    let validPass = getInputPass.value;
+    validPass = [...validPass];
+    console.log(validPass);
+    if(validPass.length>=6) {
+        err1.style.color = "#66d062";
+    } else {
+        err1.style.color = "";
+    }
+    let findNumber = validPass.find((number) => !isNaN(number));
+    if(findNumber) {
+        err2.style.color = "#66d062";
+    } else {
+        err2.style.color = "";
+    }
+})
+getInputPass2.addEventListener('change', () => {
+    let validPass = getInputPass.value;
+    let validPass2 = getInputPass2.value;
+    if(validPass !== validPass2) {
+        err7.style.color = "red";
+    } else {
+        err7.style.color = "#66d062";
+    }
+})
