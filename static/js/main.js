1985396dd8cd8d0e47ea92a0df5501e7648f7419
phat.dm <phat.dm@komit-consulting.com>
2019-07-04 13:31:31 +0700

    [FIX] Fix merge conflicts file header

--- static/js/main.js
+++ static/js/main.js
@@ -1,29 +1,29 @@
-// //Switch Tab
+// // //Switch Tab
 // var tabs = document.getElementsByClassName('tab');
 // function tabSwitch(element) {
-//     for (var i=0; i<tabs.length;i++) {
+//     for (var i = 0; i < tabs.length; i++) {
 //         tabs[i].setAttribute('class', 'tab');
 //     }
-//     element.setAttribute('class','tab active');
+//     element.setAttribute('class', 'tab active');
 // }
-// // UX/UI Validation
-// let errorList = ["Bạn phải nhập số điện thoại / mật khẩu","Số điện thoại không hợp lệ","Mật khẩu không hợp lệ"]
+// // // UX/UI Validation
+// let errorList = ["Bạn phải nhập số điện thoại / mật khẩu", "Số điện thoại không hợp lệ", "Mật khẩu không hợp lệ"]
 // let userNameElement = document.querySelector('#phone-log');
 // let passWordElement = document.querySelector('#pass-log');
 // let messengerElement = document.querySelector('.messenger');
 // function clickSubmit() {
 //     let userName = userNameElement.value;
 //     let passWord = passWordElement.value;
-//     if (!userName ||!passWord) {
-//         messengerElement.innerHTML = errorList[0] ;
+//     if (!userName || !passWord) {
+//         messengerElement.innerHTML = errorList[0];
 //         return false;
-//     }else if (userName.length !== 10) {
+//     } else if (userName.length !== 10) {
 //         messengerElement.innerHTML = errorList[1];
 //         return false;
-//     }else if (passWord.length < 6) {
-//         messengerElement.innerHTML = errorList[2] ;
+//     } else if (passWord.length < 6) {
+//         messengerElement.innerHTML = errorList[2];
 //         return false;
-//     }else {
+//     } else {
 //         return true;
 //     }
 // }
