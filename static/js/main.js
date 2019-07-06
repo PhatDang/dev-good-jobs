35726441f0a4496931a88921e930bdc77dbf5e13
duc <duc.lux@gmail.com>
2019-07-06 18:59:59 +0700

    Update UI Validation

--- static/js/main.js
+++ static/js/main.js
@@ -8,7 +8,11 @@ function outModal() {logInElem.style.display='none';regElem .style.display='none
 let errors = [];
 const getInputPass = document.querySelector("#reg_form .password");
 const getInputPass2 = document.querySelector("[name='password2']");
+const getPassLog = document.querySelector("#password");
 const getEmail = document.querySelector(".email");
+const formEl = document.querySelector('#login_form');
+const errEl = document.querySelector(".err_text");
+const getEmailLog = document.querySelector("#email");
 const getFullName = document.querySelector("#fullname");
 const getDisPlayName = document.querySelector("#display-name");
 const tooltip = document.querySelector(".tooltips");
@@ -21,6 +25,7 @@ const err6 = document.querySelector("[err='6']");
 const err7 = document.querySelector("[err='7']");
 const emailReg =  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 const fullNameReg = /[a-zA-Z ]/;
+const passWordreg = /[0-9a-zAZ@!#]/;
 getInputPass.addEventListener('focus',() => {tooltip.setAttribute('class', 'tooltips show');})
 getInputPass.addEventListener('blur', () => {tooltip.setAttribute('class', 'tooltips');})
 getInputPass.addEventListener('keyup',() => {let validPass = getInputPass.value;let i = 0;
@@ -56,3 +61,16 @@ getDisPlayName .addEventListener('blur', () => {
         getDisPlayName.style.borderColor = "#e74c3c";
     }
 })
+function checkLogErr() {
+    let email = getEmailLog.value;
+    let password = getPassLog.value;
+    if(email.length<1 || password.length<1) {
+        errEl.innerHTML = "Không được để trống email / mật khẩu";
+        event.preventDefault()
+    } else if(!(emailReg).test(email) || password.length<6) {
+        errEl.innerHTML = "Email hoặc mật khẩu không hợp lệ";
+        event.preventDefault()
+    } else {
+        return true;
+    }
+}
